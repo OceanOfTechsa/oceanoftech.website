706d53e6119cf14d22bfd19448cf3c2d959e02ad
okasi <sithuliso@oceanoftechsa.com>
2025-03-29 02:51:34 +0200

    First push -  coming soon

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Loader from "@/components/Loader";
 
-const Page = () => {
+const Home = () => {
     return (
         <div className="h-screen w-full flex flex-col justify-center items-center backdrop-blur-lg">
           <Loader />
-          <p className="text-xs font-bold">Loading view...</p>
+          <p className="text-xs font-bold">Coming soon...</p>
         </div>
     )
 }
-export default Page
+export default Home
